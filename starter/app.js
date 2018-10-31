7d846d6d85d8c7c162e1e9e734449e77406194a8
rbarker2012 <rbarker2012@gmail.com>
2018-10-31 13:35:20 -0600

    Initial Commit

--- starter/app.js
+++ starter/app.js
@@ -13,10 +13,42 @@ var scores, roundScores, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-
 activePlayer = 0;
 
-dice = Math.floor(Math.random() *6) + 1;
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // 1. need a random #
+    var dice = Math.floor(Math.random() *6) + 1;
+
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+
+    //3. Updat the round score IF the rolled number was NOT 1
+if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+} else {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+
+
+
+
+}
+});
 
 
 
@@ -29,7 +61,10 @@ dice = Math.floor(Math.random() *6) + 1;
 
 
 
-
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 
 
